6afb614bb8db0bc67be443f69a3bf774dccf8d7f
hexudong <747828522@users.github.com>
2020-08-09 18:07:47 +0800

    和旭东修改

--- src/Test01.java
+++ src/Test01.java
@@ -2,6 +2,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("MASTER HELLO WORLD!!");
         System.out.println("DEV HELLO WORLD!!");
-
+        System.out.println("和旭东修改文件");
     }
 }
